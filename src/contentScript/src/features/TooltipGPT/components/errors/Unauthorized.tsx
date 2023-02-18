6a33f97dc5d33d560e9b217857b3bc46e78113cc
Ruslan Vanin <ruslanvanin@Ruslans-MacBook-Pro.local>
2023-02-18 17:51:46 +0200

    fixed error message

--- src/contentScript/src/features/TooltipGPT/components/errors/Unauthorized.tsx
+++ src/contentScript/src/features/TooltipGPT/components/errors/Unauthorized.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Unauthorized = () => (
   <span>
-    Please log-in or log-out at{' '}
+    Please log-in or sign-up at{' '}
     <a href="https://chat.openai.com" target="_blank">
       https://chat.openai.com
     </a>
