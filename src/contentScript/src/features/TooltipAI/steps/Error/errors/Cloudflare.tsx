fe5163614669d7db769f23bc8540df8a5e395d56
NikiTays <nikitays101@gmail.com>
2023-03-02 12:49:26 +0200

    - fix bugs

--- src/contentScript/src/features/TooltipAI/steps/Error/errors/Cloudflare.tsx
+++ src/contentScript/src/features/TooltipAI/steps/Error/errors/Cloudflare.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "@mui/material";
 
 export const Cloudflare = () => (
   <span>
     Your token has been expired, please refresh it by going to{" "}
-    <a href="https://chat.openai.com" target="_blank">
+    <Link href="https://chat.openai.com" target="_blank">
       https://chat.openai.com
-    </a>{" "}
+    </Link>{" "}
     or log-in if refresh session is over
   </span>
 );
--- src/contentScript/src/features/TooltipAI/steps/Error/errors/Unauthorized.tsx
+++ src/contentScript/src/features/TooltipAI/steps/Error/errors/Unauthorized.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import { Link } from "@mui/material";
 
 export const Unauthorized = () => (
   <span>
-    Please log-in or sign-up at{' '}
-    <a href="https://chat.openai.com" target="_blank">
+    Please log-in or sign-up at{" "}
+    <Link href="https://chat.openai.com" target="_blank">
       https://chat.openai.com
-    </a>
+    </Link>
   </span>
-)
+);
